b340d406ae3b646edc6610059991f9d222dbc320
Fabien Sanchez <fzed51+git@gmail.com>
2022-02-08 01:06:46 +0100

    feat / ajout de la class de gestion des chemins

---
+++ test/FileSystem/PathTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Console\FileSystem;
+
+use PHPUnit\Framework\TestCase;
+
+class PathTest extends TestCase
+{
+    /**
+     * test l'assemblage de chemin
+     * @return void
+     */
+    public function testJoin()
+    {
+        $p = Path::join('a', 'b');
+        self::assertEquals('a/b', $p);
+    }
+
+    /**
+     * test la normalisation de chemin
+     * @return void
+     */
+    public function testNormalize()
+    {
+        self::assertEquals('a/b', Path::normalize('a\\b'), "normalise back slash");
+        self::assertEquals('a/b', Path::normalize('a\\\\b'), 'normalise double back slash');
+        self::assertEquals('a/b', Path::normalize('a/b'), 'normalise slash');
+        self::assertEquals('a/b', Path::normalize('a//b'), 'normalise double slash');
+        self::assertEquals('a/b', Path::normalize('a//\\b'), 'normalise slash et back slash');
+        self::assertEquals('a/b/c', Path::normalize('a//b\\c'), 'normalise mixed slash et back slash');
+    }
+
+    /**
+     * test du test si un chemin fini par un séparateur
+     * @return void
+     */
+    public function testEnfBySeparator()
+    {
+        self::assertTrue(Path::enfBySeparator('a/'));
+        self::assertTrue(Path::enfBySeparator('a\\'));
+        self::assertFalse(Path::enfBySeparator('a'));
+    }
+}
